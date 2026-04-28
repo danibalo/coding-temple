c2c420df5b1487d81a5448ba5a469364e5940236
Daniel Tarafa <daniyemak@gmail.com>
2026-04-28 08:43:47 -0400

    first exercise

--- prework-section-c/about_me.py
+++ prework-section-c/about_me.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+""" Declare variables with values """
 first_name = "Daniel"
 last_name = "Tarafa"
 age = "30"
@@ -9,7 +10,7 @@ fun_fact = "I escaped war and violence"
 print("=" * 40)
 print(" " * 10, "Student Profile")
 print("=" * 40)
-print(f"Name: {first_name} {last_name}\nAge: {age}\nCity: {city}\nCareer:{career}\nWhy I'm learning to code: \n {reason}\nFun fact: {fun_fact}")
+print(f"Name: {first_name} {last_name}\nAge: {age}\nCity: {city}\nCareer:{career}\nWhy I'm learning to code: \n {reason}\nFun fact: {fun_fact}") #Print in new lines
 print("=" * 40)
 
 
