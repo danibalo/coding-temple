8540ef71f0125d7a3713c198f281cae8d1eb2ec4
Daniel Tarafa <daniyemak@gmail.com>
2026-04-28 08:40:45 -0400

    first exercise

---
+++ prework-section-c/about_me.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+first_name = "Daniel"
+last_name = "Tarafa"
+age = "30"
+city = "Baltimore"
+career = "Engineer"
+reason = "I am not learning to code, i already know coding! I am here to learn AI with coding"
+fun_fact = "I escaped war and violence"
+print("=" * 40)
+print(" " * 10, "Student Profile")
+print("=" * 40)
+print(f"Name: {first_name} {last_name}\nAge: {age}\nCity: {city}\nCareer:{career}\nWhy I'm learning to code: \n {reason}\nFun fact: {fun_fact}")
+print("=" * 40)
+
+
